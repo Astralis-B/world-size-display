0484a302667b46aa9e0edfc3ab456708b50b6df2
RedstoneDisorder <julianbolisay@gmail.com>
2026-07-31 23:19:56 -0400

    Migrate to Mojang mappings and update to 26.1+

--- src/client/java/com/astralis/worldsize/WorldSizeClient.java
+++
@@ -1,8 +0,0 @@
-package com.astralis.worldsize;
-
-import net.fabricmc.api.ClientModInitializer;
-
-public class WorldSizeClient implements ClientModInitializer {
-    @Override
-    public void onInitializeClient() {}
-}
